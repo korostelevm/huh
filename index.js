ac0dddeb27d64f8f105bd2dd772969c0b63aa0b8
korostelevm <korostelevm@gmail.com>
2021-09-17 13:03:37 -0400

    j

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.get('/', (req, res) => {
   console.log(req.userAgent)
   
-    console.log("A GET reqest!")
+    console.log("A GET req!")
     res.json({Hello:'😀hhh'})
 })
 app.listen(process.env.PORT || 3000)
