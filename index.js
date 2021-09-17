69a1a11e6a4be88d24103014e5521d12ac4b6f2e
korostelevm <korostelevm@gmail.com>
2021-09-17 13:10:52 -0400

    i

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  console.log(req.userAgent)
-  
-    console.log("A GET rekhq!")
      var a = Math.random();
     console.log(a)
     res.send(a)
