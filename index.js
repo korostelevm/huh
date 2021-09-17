b29ebf7c674d7b7f8ff957acb1d81b5ee2ff1c4e
korostelevm <korostelevm@gmail.com>
2021-09-17 12:49:27 -0400

    hhg

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
+  console.log(req.userAgent)
     console.log("A GET request!")
     res.json({Hello:'😀'})
 })
