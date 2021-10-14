19ba0325a54e182bf59b4f413c7169dd600327f7
Michael Korostelev <korostelevm@gmail.com>
2021-10-14 10:58:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
      var a = Math.random();
-     console.log('recieved [GET] / ')
+     console.log('recieved [GET] /asdf ')
          
          console.log(req.headers['user-agent'])
 
