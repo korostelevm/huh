0fbdb2e942df83be3113b216e3921cf4326c56d0
Mike <korostelevm@gmail.com>
2021-11-03 11:10:45 -0400

    track

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 // index.js  
 const express = require('express')
+var Analytics = require('analytics-node');
+var analytics = new Analytics(process.env.WRITE_KEY);
 const app = express()
-app.get('/:img', (req, res) => {
+app.get('/:img',async (req, res) => {
      console.log('recieved [GET] ')
          console.log(req.params.img)
          console.log(Object.keys(req))
          console.log(req.headers['user-agent'] )
+
+
+     await  analytics.track({
+          anonymousId: 'anonymous',
+          event: `Pixel ${req.params.img}`,
+          properties: {
+               ...req.headers
+          }
+        });
 
      const trackImg = Buffer.from('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'base64');
 
